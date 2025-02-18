cee4fdf9d1ca987dedf969b591cf18886d4e27de
hope14657262 <hopemelese89@gmail.com>
2025-02-19 01:47:56 +0300

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
-
+import Home from './page/Home/Home'
 function App() {
   return (
     <>
-        <h1>Test</h1>
+       <Home/>
     </>
   )
 }
---
+++ src/page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../componet/Header/Header'
+import Footer from '../../componet/Footer/Footer'
+function Home() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
