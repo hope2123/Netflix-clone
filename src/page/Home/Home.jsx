6564ac296a046faa16c948aa7556b87653f2a607
hope14657262 <hopemelese89@gmail.com>
2025-02-19 21:37:21 +0300

    add axios.js and request.js

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../componet/Header/Header'
 import Footer from '../../componet/Footer/Footer'
+import Banner from '../../componet/Banner/Banner'
 function Home() {
   return (
     <>
     <Header/>
+    <Banner/>
     <Footer/>
     </>
   )
